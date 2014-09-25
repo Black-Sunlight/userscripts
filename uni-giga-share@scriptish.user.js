79a0b4098e968d952e01db45433dccbbe7ebc6a5
Black-Sunlight <black0swamp@gmail.com>
2014-09-26 00:50:41 +0400

    includes

--- uni-giga-share@scriptish.user.js
+++ uni-giga-share@scriptish.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @id             f918bae9-3473-4c12-85de-630d840499f0@scriptish
 // @name           unigigashare
-// @version        1.2
+// @version        1.2.1
+// @history        1.2.1 includes
 // @history        1.2 fixed: crazy reload on gigabase. Изменено: jquery библиотека, связано со взломом сайта jquery
 // @history        1.1 Добавлено: Определение отсутствующего файла и переход на страницу бесплатного скачиваниния.
 // @history        1.0 Релиз
 // @namespace      https://openuserjs.org/scripts/Black_Sun/unigigashare
 // @author         Black_Sun
 // @description    Скрипт выводит ссылку на скачивание в верху страницы. Есть настройка автоскачивания (включена по умолчанию), для выключения см. код скрипта.
-// @include        http://www.share4web.com/*
+// @include        http://www.share4web.com/get*
 // @include        http://www.unibytes.com/*
-// @include        http://www.gigabase.com/*
+// @include        http://www.gigabase.com/get*
 // @require	       https://raw.githubusercontent.com/Black-Sunlight/lib-files/master/jquery.js
 // @run-at         document-end
 // ==/UserScript==
