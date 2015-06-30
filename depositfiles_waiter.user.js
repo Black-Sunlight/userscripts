03ef0a20dd23ca949b1898bbe3d7ba9800f84e0e
Black-Sunlight <black0swamp@gmail.com>
2015-06-30 16:41:09 +0300

    new deposit version

--- depositfiles_waiter.user.js
+++ depositfiles_waiter.user.js
@@ -2,7 +2,8 @@
 // @id             depositfiles_waiter
 // @name           depositfiles_downloader
 // @namespace      https://openuserjs.org/scripts/Black_Sun/depositfiles_downloader
-// @version        4.7
+// @version        5.0
+// @history        5.0 Добавлено автоожидание при лимите и исправлена ошибка ссылок с limit=1 на конце (issue by rayman89).
 // @history        4.7 Модифицировал код под Greasemonkey, т.к. Scriptish автор походу забросил.
 // @history        4.6 Совсем убрал jQuery из скрипта и добавил GM_safeHTMLParser, спасибо KOLANICH за наводку
 // @history        4.5.0.1 Updated: Новая ссылка для обновления скрипта
@@ -55,8 +56,9 @@ else
 }
 document.addEventListener("DOMContentLoaded",function(){
 	var gate = new FormData();
+	var ss="";
 	gate.append("gateway_result", 1);
-	var info=document.querySelector("table.chousetype");
+	var info=document.querySelector("div.violation");
 	var divwait = document.createElement('div');
     divwait.innerHTML = 'Пожалуйста подождите, скачивание скоро начнётся.';
 	divwait.setAttribute('style','font-size:18px;color:green');
@@ -64,7 +66,7 @@ document.addEventListener("DOMContentLoaded",function(){
     info.parentNode.insertBefore(divwait, info);
 	var ll=location.href.split('/')[3];
 	if(ll.indexOf('files')!=-1){uli='http://dfiles.ru/'+ll+'/'+location.href.split('/')[4]}else{uli=location.href}
-	var res = GM_xmlhttpRequest(
+	GM_xmlhttpRequest(
 	{
 		method:"POST",
 		data:gate,
@@ -84,14 +86,67 @@ document.addEventListener("DOMContentLoaded",function(){
 				if (ar !== undefined) l=ar.getElementsByTagName('a')[0].getAttribute('href');
                 if (ip !== undefined)
 				{
-					document.getElementById("wait").innerHTML = ip.innerHTML;
+					clearInterval(ss)
+					var st=ip.querySelector('div.ipbg').querySelector('strong').innerHTML.replace(/(.*)(\s[0-9]{1,}\s)(минут)(.*)/ig,"$2");
+					st=st*1
+					document.getElementById("wait").innerHTML = "<i style='color:blue'>Автозапрос повторится через "+st+" минут(у), не обновляйте страницу</i><br>"+ip.innerHTML;
+					setInterval(function(){
+						if(st>0)st=st-1
+						document.getElementById("wait").innerHTML = "<i style='color:blue'>Автозапрос повторится через "+st+" минут(у), не обновляйте страницу</i><br>"+ip.innerHTML;
+					},1*60*1000)
+					var timerr=st*1*60*1000;
+				    ss=setInterval(function(){dretry()},timerr)
 				}
 				else
 				{
+					clearInterval(ss)
 					document.getElementById("wait").innerHTML = '<div style="background:lightblue;text-align:center;height:30px;"><a href="'+l+'" style="font-size: 20px;">Download</a></div>';
 					if(autodownload)location.href=l;
 				}
 			} 
 		}
 	});
+
+function dretry(){
+	GM_xmlhttpRequest(
+	{
+		method:"POST",
+		data:gate,
+		url:uli,
+		headers:{
+			"User-Agent":"Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.8.1.20) DepositFiles/FileManager 0.9.9.206",      
+			"Accept":"*/*"
+		},
+		onload:function(response) 
+		{
+			if (response.readyState == 4) 
+			{
+				var doc = new DOMParser().parseFromString(response.responseText, "text/html");
+				var ar=doc.getElementsByClassName("repeat")[0];
+				var ip=doc.getElementsByClassName("ip")[0];
+				var l='';
+				if (ar !== undefined) l=ar.getElementsByTagName('a')[0].getAttribute('href');
+                if (ip !== undefined)
+				{
+					clearInterval(ss)
+					var st=ip.querySelector('div.ipbg').querySelector('strong').innerHTML.replace(/(.*)(\s[0-9]{1,}\s)(минут)(.*)/ig,"$2");
+					st=st*1
+					document.getElementById("wait").innerHTML = "<i style='color:blue'>Ошибка, если вы видите это сообщение, значит что-то не так.<br> Автозапрос повторится через "+st+" минут(у), не обновляйте страницу</i><br>"+ip.innerHTML;
+					setInterval(function(){
+						if(st>0)st=st-1
+						document.getElementById("wait").innerHTML = "<i style='color:blue'>Ошибка, если вы видите это сообщение, значит что-то не так.<br> Автозапрос повторится через "+st+" минут(у), не обновляйте страницу</i><br>"+ip.innerHTML;
+					},1*60*1000)
+					var timerr=st*1*60*1000;
+				    ss=setInterval(function(){dretry()},timerr)
+				}
+				else
+				{
+					clearInterval(ss)
+					document.getElementById("wait").innerHTML = '<div style="background:lightblue;text-align:center;height:30px;"><a href="'+l+'" style="font-size: 20px;">Download</a></div>';
+					if(autodownload)location.href=l;
+				}
+			} 
+		}
+});
+}
 },false);
